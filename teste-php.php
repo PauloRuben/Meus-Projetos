ccf2f43086d29d0b210977f2b1ccdeeb821ff716
PauloRuben <113607399+PauloRuben@users.noreply.github.com>
2024-05-20 20:40:40 -0300

    Add files via upload

---
+++ teste-php.php
@@ -0,0 +1,83 @@
+<?php
+define("INFERIOR",1);
+define("SUPERIOR",100);
+
+$numero = rand(INFERIOR, SUPERIOR);
+echo "Meu numero da sorte: <br>";
+echo $numero."<br>";
+?>
+<?php
+$x = 5; //Ambito global
+
+function meuTeste () {
+    echo "<p>A variavel x dentro da função é: $x</p><br>";
+    }
+    //Ambito local
+         meuTeste ();
+    echo "<p>A variavel x dentro da função é: $x</p><br>";
+   
+?>
+
+<?php
+$x = 5;
+$y = 10;
+function numero(){
+    global $x, $y;
+    $y= $x + $y;
+    }
+numero();
+echo $y;
+?>
+
+<?php
+//ambito estatico
+function teste(){
+        static $x = 0;
+        echo $x;
+        $x++;
+    }
+echo "<br>";
+echo "<br>";
+teste();
+echo "<br>";
+teste();
+echo "<br>";
+teste();
+
+?>
+<?php
+echo "<br>Geovane da Costa";
+print "<br>Geovane da Costa<br><br>";
+?>
+<?php
+$x = 5;
+var_dump($x);
+echo "<br>";
+?>
+<?php
+$x = 'Minha string';
+$y = "Minha string 2!";
+var_dump($x);
+echo "<br>";
+var_dump($y);
+echo "<br>";
+?>
+<?php
+$x = 3.14;
+var_dump($x);
+echo "<br>";
+?>
+<?php
+$x = true;
+var_dump($x);
+echo "<br>";
+?>
+<?php
+$cars =array("BMW","BYD","AUDI");
+var_dump($cars);
+echo "<br>";
+?>
+<?php
+echo"<br>"
+echo strlen("Olá Mundo")
+?>
